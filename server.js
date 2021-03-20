773d9d49ec12e9fe9dc1bc6fdc40d2b42fc7d60e
besianzhu <besianzhu@qq.com>
2021-03-20 10:22:59 +0800

    禁止空提交

--- server.js
+++ server.js
@@ -1,55 +1,55 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const server = require('http').createServer(app);
-const io = require('socket.io').listen(server).sockets;
+const server = require("http").createServer(app);
+const io = require("socket.io").listen(server).sockets;
 
-
-app.get('/',(req,res) => {
-    res.sendFile(__dirname+'/index.html');
-
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
 });
 
-let connectedUser =[];
-io.on("connection", socket =>{
-    console.log("a user connected");
-    updateUserName();
-    let userName = '';
+let connectedUser = [];
+io.on("connection", (socket) => {
+  console.log("a user connected");
+  updateUserName();
+  let userName = "";
 
-    //login
-    socket.on('login', (name, callback) =>{
-        if(name.trim().length ===0){
-            return;
-        }
-        callback(true);
-        userName= name;
-        connectedUser.push(userName);
-        // console.log(connectedUser);
-        updateUserName();
-    })
-    //receive chat message
-    socket.on('chat message', msg =>{
-        console.log(msg);
-        //emit message data
-        io.emit("output", {
-            name:userName,
-            msg:msg
-        });
-    })
-    //disconnect
-    socket.on('disconnect', () =>{
-        console.log('disconneted');
-        connectedUser.splice(connectedUser.indexOf(userName),1);
-        console.log(connectedUser);
-        updateUserName();
-    });
-
-    function updateUserName(){
-        io.emit('loadUser',connectedUser);
+  //login
+  socket.on("login", (name, callback) => {
+    if (name.trim().length === 0) {
+      return;
     }
-})
+    callback(true);
+    userName = name;
+    connectedUser.push(userName);
+    // console.log(connectedUser);
+    updateUserName();
+  });
+  //receive chat message
+  socket.on("chat message", (msg) => {
+    console.log(msg);
+    if (msg.trim().length === 0) {
+      return;
+    }
+    //emit message data
+    io.emit("output", {
+      name: userName,
+      msg: msg,
+    });
+  });
+  //disconnect
+  socket.on("disconnect", () => {
+    console.log("disconneted");
+    connectedUser.splice(connectedUser.indexOf(userName), 1);
+    console.log(connectedUser);
+    updateUserName();
+  });
 
-const port = process.env.PORT || 5000;
-server.listen(port , () =>{
-    console.log('server running...');
+  function updateUserName() {
+    io.emit("loadUser", connectedUser);
+  }
 });
 
+const port = process.env.PORT || 2333;
+server.listen(port, () => {
+  console.log("server running @ http://localhost:2333");
+});
